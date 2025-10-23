825ec05c85eaa541fb464de559430cfdc13cbd39
brobi619 <appdev619@gmail.com>
2025-10-22 21:42:58 -0700

    updated API files

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target:
+          process.env.NODE_ENV === 'production'
+            ? 'https://fhpe-backend.onrender.com'
+            : 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },
